a483548001a3796bb6d0f16f3c6865a2f5dfd7ac
Abner <calvac100@gmail.com>
2020-07-10 22:14:42 -0600

    docs agregados

---
+++ docs/precache-manifest.6a6d86714cc0886931aefb9afdc8238e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b7db3908ca6710454139b70d9dc006",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb61516b6225a96209f8",
+    "url": "/static/css/main.b577c49c.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb61516b6225a96209f8",
+    "url": "/static/js/main.1089c479.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
